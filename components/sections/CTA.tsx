bb18359dc8b486d2376aa73071cd0c0341a57984
zoyaa26 <nnglideon@gmail.com>
2026-09-19 18:28:39 +0800

    modified cta button

--- components/sections/CTA.tsx
+++ components/sections/CTA.tsx
@@ -30,13 +30,6 @@ export default function CTA() {
             >
               Mulai Konsultasi
             </a>
-
-            <a
-              href="#portfolio"
-              className="w-full rounded-xl border border-sky-200 bg-white px-7 py-4 font-semibold text-sky-600 transition duration-300 hover:-translate-y-0.5 hover:border-sky-300 hover:bg-sky-50 sm:w-auto"
-            >
-              Lihat Portofolio
-            </a>
           </div>
         </div>
       </Reveal>
